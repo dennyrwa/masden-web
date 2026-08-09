1ac5da7de6c318acc7f82c51187a949af0095349
Denny Rakhmad Widi Ashari <dennyrwa@gmail.com>
2026-08-09 11:48:39 +0700

    Save my local firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,19 +1,20 @@
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 // src/firebase/init.js
 // Menggunakan Firebase SDK versi 10 (Modular) untuk menghemat ukuran file (Performance Strategy)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 // Konfigurasi ini nantinya Bapak dapatkan dari Firebase Console
 const firebaseConfig = {
-  apiKey: "API_KEY_BAPAK",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyC-ewFHd9lCp0ZDyAkEKu2bA_AcQBfx8R4",
+  authDomain: "masden-web.firebaseapp.com",
+  projectId: "masden-web",
+  storageBucket: "masden-web.firebasestorage.app",
+  messagingSenderId: "781504676708",
+  appId: "1:781504676708:web:ab76071db3e77f461ef071",
+  measurementId: "G-9F5CQLNGWF",
 };
-
+https://github.com/dennyrwa/masden-web.git
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -21,4 +22,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-console.log("Sistem SINTA: Firebase berhasil diinisialisasi dengan mode hemat kuota.");
+console.log(
+  "Sistem SINTA: Firebase berhasil diinisialisasi dengan mode hemat kuota.",
+);
